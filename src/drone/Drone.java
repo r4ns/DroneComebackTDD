f7d55223c68df02f60bf9d34357c6088c6eaeafd
Robert Ramac <robert3ns@gmail.com>
2017-05-25 16:16:56 +0200

    Initial commit

---
+++ src/drone/Drone.java
@@ -0,0 +1,47 @@
+package drone;
+
+public class Drone implements StandardDrone{
+
+	@Override
+	public String moveUp() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String moveDown() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String moveLeft() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String moveRight() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String moveBack() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String moveForth() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getFormatedCoordinates() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
