b56e870c24756abcb6dcee68b553bc99c924297f
somename <someemail>
2017-05-25 19:57:15 +0200

    besouro automatic message

--- src/tests/DroneTest.java
+++ src/tests/DroneTest.java
@@ -1,5 +1,26 @@
 package tests;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import drone.Drone;
+
 public class DroneTest {
+	Drone drone;
+	private int[] boundaries={50,50,50};
+	private int[] startCoordinates={30,0,30};
+	
+	@Test
+	public void test_MoveUp(){
+		drone= new Drone(boundaries, startCoordinates);
+		String expectedCoordinates="("
+				+Integer.toString(startCoordinates[0])+","
+		+Integer.toString(startCoordinates[1]+1)+","
+		+Integer.toString(startCoordinates[2]) + ")";
+		assertEquals(expectedCoordinates,drone.moveUp());
+		
+		
+	}
 
 }
