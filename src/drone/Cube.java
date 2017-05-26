7e60951b192ffcc2cbb859f893eeaff1c363981e
SmArt User <SmArt User@DESKTOP-214PFU1>
2017-05-26 12:50:32 +0200

    DroneComebackTDD

--- src/drone/Cube.java
+++ src/drone/Cube.java
@@ -1,5 +1,38 @@
 package drone;
 
 public class Cube {
+	protected int[] startCoordinates;
+	protected int length;
+	
+	public Cube() {
+		
+	}
+	
+	public Cube (int[] startCoordinates, int length) {
+		this.startCoordinates = startCoordinates;
+		this.length = length;
+	}
+	
+	public String toString() {
+		return "startCoordinates: {" + Integer.toString(this.startCoordinates[0]) +
+				", " + Integer.toString(this.startCoordinates[1]) + 
+				", " + Integer.toString(this.startCoordinates[2]) +
+				"}, length: " + Integer.toString(this.length);
+	}
 
+	public int[] getStartCoordinates() {
+		return startCoordinates;
+	}
+
+	public void setStartCoordinates(int[] startCoordinates) {
+		this.startCoordinates = startCoordinates;
+	}
+
+	public int getLength() {
+		return length;
+	}
+
+	public void setLength(int length) {
+		this.length = length;
+	}
 }
--- src/tests/CubeTest.java
+++ src/tests/CubeTest.java
@@ -1,5 +1,18 @@
 package tests;
 
-public class CubeTest {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
+
+import drone.Cube;
+
+public class CubeTest {
+	@Test
+	public void test_CreatingCube() {
+		Cube cube = new Cube(new int[] {0, 0, 0}, 50);
+		
+		String expected = "startCoordinates: {0, 0, 0}, length: 50";
+		
+		assertEquals(expected, cube.toString());
+	}
 }
