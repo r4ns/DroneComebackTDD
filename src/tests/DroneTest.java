48abd9f3fd45e8a94e551ec4b21e7677374fa3bb
itc3 <itc3@ws1.itc03.edu>
2017-05-26 20:40:31 +0200

    it8-2015

--- src/tests/DroneTest.java
+++ src/tests/DroneTest.java
@@ -1,5 +1,117 @@
 package tests;
 
-public class DroneTest {
+import static org.junit.Assert.*;
 
+import org.junit.Test;
+
+import drone.Drone;
+
+public class DroneTest {
+	@Test
+	public void test_CreatingDrone() {
+		Drone drone = new Drone(0, 0, 0);
+		
+		String expected = "Drone: (0, 0, 0)";
+		
+		assertEquals(expected, drone.getFormatedCoordinates());
+	}
+	
+	@Test
+	public void test_InitialMoveUp() {
+		Drone drone = new Drone(0, 0, 0);
+		
+		String expected = "Drone: (0, 1, 0)";
+		
+		assertEquals(expected, drone.moveUp());
+	}
+	
+	@Test
+	public void test_BoundariesMoveUp() {
+		Drone drone = new Drone(20, 10, 20);
+		
+		String expected = "Drone: (20, 10, 20)";
+		
+		assertEquals(expected, drone.moveUp());
+	}
+	@Test
+	public void test_InitialMoveDown() {
+		Drone drone = new Drone(0, 1, 0);
+		
+		String expected = "Drone: (0, 0, 0)";
+		
+		assertEquals(expected, drone.moveDown());
+	}
+	@Test
+	public void test_BoundariesMoveDown() {
+		Drone drone = new Drone(20, 10, 20);
+		
+		String expected = "Drone: (20, 10, 20)";
+		
+		assertEquals(expected, drone.moveDown());
+	}
+	@Test
+	public void test_InitialMoveLeft() {
+		Drone drone = new Drone(1, 0, 0);
+		
+		String expected = "Drone: (0, 0, 0)";
+		
+		assertEquals(expected, drone.moveLeft());
+	}
+	@Test
+	public void test_BoundariesMoveLeft() {
+		Drone drone = new Drone(20, 10, 20);
+		
+		String expected = "Drone: (19, 10, 20)";
+		
+		assertEquals(expected, drone.moveLeft());
+	}
+	@Test
+	public void test_InitialMoveRight() {
+		Drone drone = new Drone(0, 0, 0);
+		
+		String expected = "Drone: (1, 0, 0)";
+		
+		assertEquals(expected, drone.moveRight());
+	}
+	@Test
+	public void test_BoundariesMoveRight() {
+		Drone drone = new Drone(20, 10, 20);
+		
+		String expected = "Drone: (21, 10, 20)";
+		
+		assertEquals(expected, drone.moveRight());
+	}
+	@Test
+	public void test_InitialMoveBack() {
+		Drone drone = new Drone(0, 0, 0);
+		
+		String expected = "Drone: (0, 0, 1)";
+		
+		assertEquals(expected, drone.moveBack());
+	}
+	@Test
+	public void test_BoundariesMoveBack() {
+		Drone drone = new Drone(20, 10, 20);
+		
+		String expected = "Drone: (20, 10, 21)";
+		
+		assertEquals(expected, drone.moveBack());
+	}
+	@Test
+	public void test_InitialMoveForth() {
+		Drone drone = new Drone(0, 0, 1);
+		
+		String expected = "Drone: (0, 0, 0)";
+		
+		assertEquals(expected, drone.moveForth());
+	}
+	@Test
+	public void test_BoundariesMoveForth() {
+		Drone drone = new Drone(20, 10, 20);
+		
+		String expected = "Drone: (20, 10, 19)";
+		
+		assertEquals(expected, drone.moveForth());
+	}
+	
 }
