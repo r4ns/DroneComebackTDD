5831139a29608aab1fb3ccd2ea9d505b69527124
ZoricaBabic <zorica.b96@gmail.com>
2017-05-31 11:48:55 +0200

    testiranje

--- src/drone/Drone.java
+++ src/drone/Drone.java
@@ -1,47 +1,62 @@
 package drone;
 
 public class Drone implements StandardDrone{
+	
+	private int x;
+	private int y;
+	private int z;
+	
+	public Drone(int x, int y, int z){
+		
+		this.x=x;
+		this.y=y;
+		this.z=z;
+	}
 
 	@Override
 	public String moveUp() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		y++;
+		return getFormatedCoordinates();
 	}
 
 	@Override
 	public String moveDown() {
-		// TODO Auto-generated method stub
-		return null;
+		y--;
+		return getFormatedCoordinates();
 	}
 
 	@Override
 	public String moveLeft() {
-		// TODO Auto-generated method stub
-		return null;
+		x--;
+		
+		return getFormatedCoordinates();
 	}
 
 	@Override
 	public String moveRight() {
-		// TODO Auto-generated method stub
-		return null;
+		x++;
+		return getFormatedCoordinates();
 	}
 
 	@Override
 	public String moveBack() {
-		// TODO Auto-generated method stub
-		return null;
+		z--;
+		return getFormatedCoordinates();
 	}
 
 	@Override
 	public String moveForth() {
-		// TODO Auto-generated method stub
-		return null;
+		z++;
+		return getFormatedCoordinates();
 	}
 
 	@Override
 	public String getFormatedCoordinates() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return "Drone position: (" + x + "," + y + "," + z + ")";
+		
+		
 	}
 
 }
