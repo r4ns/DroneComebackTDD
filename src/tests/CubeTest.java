50e42790fea13b4746109af0fa5ffd609b2a222d
DAMJAN <DAMJAN@Damjan>
2017-06-02 01:29:16 +0200

    IT46-2015

--- src/tests/CubeTest.java
+++ src/tests/CubeTest.java
@@ -1,5 +1,13 @@
 package tests;
 
+import drone.Cube;
+
 public class CubeTest {
 
+	@Test
+	public void test_CubeConstructor() {
+		Cube cube = new Cube(new int[] {30,0,30}, 10);
+		assertNotNull(cube);
+	}
+	
 }
--- src/tests/DroneTest.java
+++ src/tests/DroneTest.java
@@ -1,5 +1,58 @@
 package tests;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import drone.Drone;
+
 public class DroneTest {
 
+	@Test
+	public void test_DroneStartPosition() {
+		int[] startCoordinates = new int[] {30,0,30};
+		Drone drone = new Drone(startCoordinates);
+		String coordinates = drone.getFormatedCoordinates();
+		assertEquals("(30,0,30)", coordinates);
+	}
+	
+	@Test
+	public void test_DroneInitialMoveUp() {
+		int[] startCoordinates = new int[] {30,0,30};
+		Drone drone = new Drone(startCoordinates);
+		drone.moveUp();
+		String coordinates = drone.getFormatedCoordinates();
+		assertEquals("(30,1,30)", coordinates);
+	}
+	
+	@Test
+	public void test_DroneInitialMoveDown() {
+		int[] startCoordinates = new int[] {30,5,30};
+		Drone drone = new Drone(startCoordinates);
+		drone.moveDown();
+		String coordinates = drone.getFormatedCoordinates();
+		assertEquals("(30,4,30)", coordinates);
+	}
+	
+	
+	@Test
+	public void test_DroneInitialMoveRight() {
+		int[] startCoordinates = new int[] {26,0,30};
+		Drone drone = new Drone(startCoordinates);
+		drone.moveRight();
+		String coordinates = drone.getFormatedCoordinates();
+		assertEquals("(27,0,30)", coordinates);
+	}
+	
+	@Test
+	public void test_DroneInitialMoveBack() {
+		int[] startCoordinates = new int[] {30,0,5};
+		Drone drone = new Drone(startCoordinates);
+		drone.moveBack();
+		String coordinates = drone.getFormatedCoordinates();
+		assertEquals("(30,0,6)", coordinates);
+	}
+	
+
+	
 }
