0bf32ee2b9d91fd1b0db6db247734ea6c15e9b81
somename <someemail>
2017-06-01 09:55:58 +0200

    besouro automatic message

--- src/tests/DroneTest.java
+++ src/tests/DroneTest.java
@@ -16,4 +16,12 @@ public class DroneTest {
 		String expected = "Drone position is: (0, 1, 0)";
 		assertEquals(expected, dr.getFormatedCoordinates());
 	}
+	
+	@Test 
+	public void test_initialMoveDown() throws DroneException {
+		Drone dr = new Drone(new Cube(new int[]{0,0,0}, 50), new Cube(new int[]{10,10,10}, 30), new int[]{0,0,0});
+		dr.moveUp();
+		String expected = "Drone position is: (0, 1, 0)";
+		assertEquals(expected, dr.getFormatedCoordinates());
+	}
 }
