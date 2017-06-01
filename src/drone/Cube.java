560b49f8833e97fc839715245f58f77df23b8029
cecaKat22 <User@User-NB>
2017-06-01 10:43:25 +0200

    droneComeBack

--- src/drone/Cube.java
+++ src/drone/Cube.java
@@ -1,5 +1,37 @@
 package drone;
 
 public class Cube {
-
+	
+	private int[] maxKoordinate;
+	private int stranica;
+	private int[] minKoordinate;
+	
+	public Cube(){
+		
+	}
+	public Cube(int[] pocetneKoordinate,int stranica){
+		
+		this.minKoordinate=new int[]{pocetneKoordinate[0],pocetneKoordinate[1],pocetneKoordinate[2]};
+		this.stranica=stranica;
+		this.maxKoordinate=new int[]{pocetneKoordinate[0]+stranica,pocetneKoordinate[1]+stranica,pocetneKoordinate[2]+stranica};
+	}
+	
+	public int[] getMaxKoordinate() {
+		return maxKoordinate;
+	}
+	public int getStranica() {
+		return stranica;
+	}
+	public int[] getMinKoordinate() {
+		return minKoordinate;
+	}
+	public void setMaxKoordinate(int[] maxKoordinate) {
+		this.maxKoordinate = maxKoordinate;
+	}
+	public void setStranica(int stranica) {
+		this.stranica = stranica;
+	}
+	public void setMinKoordinate(int[] minKoordinate) {
+		this.minKoordinate = minKoordinate;
+	}
 }
--- src/tests/DroneTest.java
+++ src/tests/DroneTest.java
@@ -1,5 +1,49 @@
 package tests;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import drone.Drone;
+
 public class DroneTest {
 
+	@Test 
+	public void Drone_StartPosition(){
+		
+		int[] pocetnaPozicija=new int[]{30,0,30};
+		Drone dron=new Drone(pocetnaPozicija);
+		String pozicija=dron.getFormatedCoordinates();
+		assertEquals("(30,0,30)",pozicija);
+	}
+	
+	@Test
+	public void Drone_InitialMoveUp(){
+		
+		int[] pocetnaPozicija=new int[]{30,0,30};
+		Drone dron=new Drone(pocetnaPozicija);
+		dron.moveUp();
+		String pozicija=dron.getFormatedCoordinates();
+		assertEquals("(30,1,30)",pozicija);
+	}
+	
+	@Test
+	public void Drone_InitialMoveDown(){
+		
+		int[] pocetnaPozicija=new int[]{30,1,30};
+		Drone dron=new Drone(pocetnaPozicija);
+		dron.moveDown();
+		String pozicija=dron.getFormatedCoordinates();
+		assertEquals("(30,0,30)",pozicija);
+	}
+	@Test
+	public void Drone_InitialMoveBack(){
+		
+		int[] pocetnaPozicija=new int[]{30,1,30};
+		Drone dron=new Drone(pocetnaPozicija);
+		dron.moveBack();
+		String pozicija=dron.getFormatedCoordinates();
+		assertEquals("(30,1,31)",pozicija);
+	}
+	
 }
