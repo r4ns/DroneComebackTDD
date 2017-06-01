76a6ed6f39e7ea8dcb4c52072f7b19c1e177bc32
Dijana <Dijana@Inspiron>
2017-06-02 01:24:33 +0200

    Done

--- src/drone/Drone.java
+++ src/drone/Drone.java
@@ -2,46 +2,123 @@ package drone;
 
 public class Drone implements StandardDrone{
 
+	private int x;
+	private int y;
+	private int z;
+	FlySpace space;
+	
+	public Drone(){
+		
+	}
+	public Drone(int x,int y,int z){
+		this.x=x;
+		this.y=y;
+		this.z=z;
+	}
+	public Drone(int x, int y, int z, Cube spoljasnja, Cube unutrasnja) {
+		this.x=x;
+		this.y=y;
+		this.z=z;
+		this.space=new FlySpace(spoljasnja,unutrasnja);	
+	}
+	public String toString(){
+		
+		return "Drone position: ("+this.x+","+this.y+","+this.z+")";
+	}
+
 	@Override
 	public String moveUp() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		if(y<space.getUnutrasnjaKocka().getBoundaries()[1]&&y>=space.getSpoljasnjaKocka().getBoundaries()[1]
+				&&x>space.getUnutrasnjaKocka().getBoundaries()[0]&&x<(space.getUnutrasnjaKocka().getBoundaries()[0]+space.getUnutrasnjaKocka().getStranica()))
+		{
+			y++;
+		}
+		if(y>=(space.getUnutrasnjaKocka().getBoundaries()[1]+space.getUnutrasnjaKocka().getStranica())
+				&&y<(space.getSpoljasnjaKocka().getBoundaries()[1]+space.getSpoljasnjaKocka().getStranica())
+				&&x>space.getUnutrasnjaKocka().getBoundaries()[0]&&x<(space.getUnutrasnjaKocka().getBoundaries()[0]+space.getUnutrasnjaKocka().getStranica())
+				)
+		{
+			y++;
+		}
+		
+		return this.toString();
+		
 	}
 
 	@Override
 	public String moveDown() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		if(y<=space.getUnutrasnjaKocka().getBoundaries()[1]&&y>space.getSpoljasnjaKocka().getBoundaries()[1])
+		{
+			y--;
+		}
+		if(y>(space.getUnutrasnjaKocka().getBoundaries()[1]+space.getUnutrasnjaKocka().getStranica())
+				&&y<=(space.getSpoljasnjaKocka().getBoundaries()[1]+space.getSpoljasnjaKocka().getStranica()))
+		{
+			y--;
+		}
+		
+		
+		return this.toString();
 	}
 
 	@Override
 	public String moveLeft() {
-		// TODO Auto-generated method stub
-		return null;
+		if(x>space.getSpoljasnjaKocka().getBoundaries()[0]&&x<=(space.getSpoljasnjaKocka().getStranica())
+				&&(y<=space.getUnutrasnjaKocka().getBoundaries()[1]||y>=(space.getUnutrasnjaKocka().getStranica())))
+		{
+			x--;
+		}
+		
+		return this.toString();
 	}
 
 	@Override
 	public String moveRight() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		if(x>=space.getSpoljasnjaKocka().getBoundaries()[0]&&x<(space.getSpoljasnjaKocka().getStranica())
+				&&(y<=space.getUnutrasnjaKocka().getBoundaries()[1]||y>=(space.getUnutrasnjaKocka().getStranica())))
+		{
+			x++;
+		}
+		if(((x>=space.getSpoljasnjaKocka().getBoundaries()[0]&&x<space.getSpoljasnjaKocka().getBoundaries()[0]) ||
+				(x>=(space.getUnutrasnjaKocka().getStranica())&&x<space.getSpoljasnjaKocka().getStranica()))
+				&&( y>=space.getUnutrasnjaKocka().getBoundaries()[1] || ( y<=space.getUnutrasnjaKocka().getStranica())))
+		{
+			x++;
+		}
+		
+		return this.toString();
 	}
 
 	@Override
 	public String moveBack() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		if(z<space.getSpoljasnjaKocka().getStranica()&&z>=space.getSpoljasnjaKocka().getBoundaries()[2]){
+			z++;
+		}
+		if((z>=space.getSpoljasnjaKocka().getBoundaries()[2]&&z<space.getUnutrasnjaKocka().getBoundaries()[2] || 
+				z>=(space.getUnutrasnjaKocka().getBoundaries()[2]+space.getUnutrasnjaKocka().getStranica())&&z<space.getSpoljasnjaKocka().getStranica()))
+		{
+			z++;
+		}
+		
+		return this.toString();
 	}
 
 	@Override
 	public String moveForth() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		if(z<=space.getSpoljasnjaKocka().getStranica()&&z>space.getSpoljasnjaKocka().getBoundaries()[2]){
+			z--;
+		}
+		return this.toString();
 	}
 
 	@Override
 	public String getFormatedCoordinates() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.toString();
 	}
 
 }
