67fac65b6b33bef4d30269c2814450e9e5eca0c1
gavri <gavri@LAPTOP-T8C9NG9S>
2017-06-01 11:27:28 +0200

    DroneComeback TDD

--- src/drone/FlySpace.java
+++ src/drone/FlySpace.java
@@ -2,4 +2,28 @@ package drone;
 
 public class FlySpace {
 
+	private Cube outerCube;
+	private Cube innerCube;
+	
+	public FlySpace(Cube outer, Cube inner) {
+		outerCube = outer;
+		innerCube = inner;
+	}
+
+	public Cube getOuterCube() {
+		return outerCube;
+	}
+
+	public Cube getInnerCube() {
+		return innerCube;
+	}
+
+	public void setOuterCube(Cube outerCube) {
+		this.outerCube = outerCube;
+	}
+
+	public void setInnerCube(Cube innerCube) {
+		this.innerCube = innerCube;
+	}
+	
 }
