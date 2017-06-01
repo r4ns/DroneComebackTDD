035bc5616c1f4558f92b8ccb9e5e4a02f02c15bf
Miroslav <Miroslav@192.168.0.17>
2017-06-01 22:11:03 +0200

    IT51-2015

--- src/drone/FlySpace.java
+++ src/drone/FlySpace.java
@@ -1,5 +1,34 @@
 package drone;
-
+ 
 public class FlySpace {
-
+   
+    protected Cube outerBoundaries;
+    protected Cube innerBoundaries;
+    
+    public FlySpace()
+    {
+    	
+    }
+   
+    public FlySpace(Cube outerCube, Cube innerCube)
+    {
+        outerBoundaries = outerCube;
+        innerBoundaries = innerCube;
+    }
+ 
+    public Cube getOuterBoundaries() {
+        return outerBoundaries;
+    }
+ 
+    public void setOuterBoundaries(Cube outerBoundaries) {
+        this.outerBoundaries = outerBoundaries;
+    }
+ 
+    public Cube getInnerBoundaries() {
+        return innerBoundaries;
+    }
+ 
+    public void setInnerBoundaries(Cube innerBoundaries) {
+        this.innerBoundaries = innerBoundaries;
+    }
 }
