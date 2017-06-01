b830e905770303e0a2062cfcad01446dbcfcf5a2
Igor <Igor@192.168.0.10>
2017-06-01 23:10:02 +0200

    IT42-2015

--- src/tests/CubeTest.java
+++ src/tests/CubeTest.java
@@ -1,5 +1,28 @@
 package tests;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import drone.Cube;
+import drone.DroneException;
+
 public class CubeTest {
+	
+	@Test
+	public void test_konstruktor_cube(){
+  		Cube cube = new Cube();
+  		
+  		assertNotNull(cube);
+  	}
+	
+	@Test
+	public void test_konstruktor_cube_sa_parametrima()
+  	{
+		int [] cubeStartCoordinates = null; 
+		int side = 0;
+  		Cube cube = new Cube(cubeStartCoordinates, side);
+  		assertNotNull(cube);
+  	}
 
 }
