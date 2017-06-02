9ee8ba8621a66169e05460de30358984a44a34a9
Nikola PC <Nikola PC@Nikola>
2017-06-02 11:13:40 +0200

    Nikola Zivkovic IT-41

--- src/drone/Cube.java
+++ src/drone/Cube.java
@@ -1,5 +1,50 @@
 package drone;
 
 public class Cube {
-
+	private int[] granice;
+	private int stranica;
+	
+	public Cube(){
+		
+	}
+	
+	public Cube (int[] boundaries,int stranica){
+		this.stranica=stranica;
+		this.granice=boundaries;
+	}
+	
+	public String getStartCoordinates() {
+		return "("+granice[0]+","+granice[1]+","+granice[2]+")";
+	}
+	
+	public boolean isInsideCube(Cube cube) {
+		if(this.granice[0]>cube.getGranice()[0] && this.granice[1]>cube.getGranice()[1] && this.granice[2]>cube.getGranice()[2]){
+			return true;
+		}else{
+			return false;
+		}
+			
+	}
+	
+	public String toString(){
+		return "Cube position: "+this.getStartCoordinates()+", length: "+this.stranica;
+	}
+	
+	public void setStranica(int stranica) {
+		this.stranica = stranica;
+	}
+	
+	public int getStranica() {
+		return stranica;
+	}
+	
+	public void setGranice(int[] granice) {
+		this.granice = granice;
+	}
+	
+	public int[] getGranice() {
+		return granice;
+	}
+	
+	
 }
--- src/tests/CubeTest.java
+++ src/tests/CubeTest.java
@@ -1,5 +1,26 @@
 package tests;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import drone.Cube;
+
 public class CubeTest {
 
+	@Test
+	public void testCubeOut(){
+		Cube out=new Cube(new int[] {0,0,0},50);
+		
+		String expected="Cube position: (0,0,0), length: 50";
+		
+		assertEquals(expected,out.toString());
+	}
+	
+	@Test
+	public void testCubesInAndOut(){
+		Cube spolj=new Cube(new int[] {0,0,0},50);
+		Cube unut=new Cube(new int[] {10,10,10},30);
+		assertTrue(unut.isInsideCube(spolj));
+	}
 }
