4a82c7b15ef0348f31588a733d13e956b4a6eef0
Bacic <Bacic@DESKTOP-CSFNDM5>
2017-06-02 02:40:57 +0200

    commit

--- src/drone/Cube.java
+++ src/drone/Cube.java
@@ -2,4 +2,40 @@ package drone;
 
 public class Cube {
 
+	private int[] minCoordinates;
+	private int[] maxCoordinates;
+	private int lenghtCube;
+	
+	public Cube(int[] startCoordinates, int size) {
+		minCoordinates = new int[]{startCoordinates[0],startCoordinates[1],startCoordinates[2]};
+		lenghtCube = size;
+		maxCoordinates = new int[]{startCoordinates[0] + size,startCoordinates[1] + size,startCoordinates[2] + size};
+	}
+
+	public int[] getMinCoordinates() {
+		return minCoordinates;
+	}
+
+	public int[] getMaxCoordinates() {
+		return maxCoordinates;
+	}
+
+	public int getLenghtCube() {
+		return lenghtCube;
+	}
+
+	public void setMinCoordinates(int[] minCoordinates) {
+		this.minCoordinates = minCoordinates;
+	}
+
+	public void setMaxCoordinates(int[] maxCoordinates) {
+		this.maxCoordinates = maxCoordinates;
+	}
+
+	public void setLenghtCube(int lenghtCube) {
+		this.lenghtCube = lenghtCube;
+	}
+	
+	
+	
 }
