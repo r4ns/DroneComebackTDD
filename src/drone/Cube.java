b73f4854deabd9ac29887e5adefa49a998d74646
srdjanLL <lulic.ing@gmail.com>
2017-06-02 15:35:08 +0200

    Drone TDD all tests implemented

--- src/drone/Cube.java
+++ src/drone/Cube.java
@@ -1,5 +1,21 @@
 package drone;
 
 public class Cube {
+	
+	public int[] minCoordinates;
+	public int[] maxCoordinates;
+	int side;
+	public Cube(int[] array, int side) {
+		// TODO Auto-generated constructor stub
+		minCoordinates = array;
+		maxCoordinates = new int[minCoordinates.length];
+		for (int i = 0; i < minCoordinates.length; i++)
+			maxCoordinates[i] = minCoordinates[i] + side;
+		this.side = side;
+		
+	}
+	public String toString(){
+		return "Start coordinates:({"+ minCoordinates[0] +", "+ minCoordinates[1] +", "+ minCoordinates[2] +"}, "+ side + ")";
+	}
 
 }
