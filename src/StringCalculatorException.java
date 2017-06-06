cef7f179544433fb8f3741b3ea50d217fe320f98
MILOS <misalin333@gmail.com>
2017-06-06 11:57:10 +0200

    StringCalculatorTDD

---
+++ src/StringCalculatorException.java
@@ -0,0 +1,9 @@
+
+public class StringCalculatorException extends Exception {
+	public StringCalculatorException(){
+		 		super();
+		 	}
+		 	public StringCalculatorException(String poruka){
+		 		super(poruka);
+		 	}
+} 
